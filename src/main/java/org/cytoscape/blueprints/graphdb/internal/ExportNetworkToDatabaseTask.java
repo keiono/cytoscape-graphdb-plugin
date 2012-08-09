87ff5647429a4a79f95ab502f3a01fae22b13d64
Keiichiro Ono <keiono@gmail.com>
2012-08-09 00:44:53 -0700

    TODO: Implement these tasks.

---
+++ src/main/java/org/cytoscape/blueprints/graphdb/internal/ExportNetworkToDatabaseTask.java
@@ -0,0 +1,22 @@
+package org.cytoscape.blueprints.graphdb.internal;
+
+import java.util.Collection;
+
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.task.AbstractNetworkCollectionTask;
+import org.cytoscape.work.TaskMonitor;
+
+public class ExportNetworkToDatabaseTask extends AbstractNetworkCollectionTask {
+
+	public ExportNetworkToDatabaseTask(Collection<CyNetwork> networks) {
+		super(networks);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run(TaskMonitor taskMonitor) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/org/cytoscape/blueprints/graphdb/internal/ImportFullGraphFromDatabaseTask.java
@@ -0,0 +1,14 @@
+package org.cytoscape.blueprints.graphdb.internal;
+
+import org.cytoscape.work.AbstractTask;
+import org.cytoscape.work.TaskMonitor;
+
+public class ImportFullGraphFromDatabaseTask extends AbstractTask {
+
+	@Override
+	public void run(TaskMonitor taskMonitor) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
