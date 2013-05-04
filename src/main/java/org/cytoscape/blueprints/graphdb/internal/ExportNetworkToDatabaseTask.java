b1ca10e0a9d3609793c035ce9fc4e9b571bd993e
Keiichiro Ono <keiono@gmail.com>
2013-05-03 19:10:06 -0700

    Basic export function works now.

--- src/main/java/org/cytoscape/blueprints/graphdb/internal/ExportNetworkToDatabaseTask.java
+++ src/main/java/org/cytoscape/blueprints/graphdb/internal/ExportNetworkToDatabaseTask.java
@@ -1,16 +1,18 @@
 package org.cytoscape.blueprints.graphdb.internal;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-
-import javax.xml.transform.Source;
 
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.model.CyColumn;
 import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyIdentifiable;
+import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNode;
 import org.cytoscape.model.CyRow;
 import org.cytoscape.model.CyTable;
@@ -19,13 +21,23 @@ import org.cytoscape.work.ProvidesTitle;
 import org.cytoscape.work.TaskMonitor;
 import org.cytoscape.work.Tunable;
 
+import scala.xml.NodeSeq;
+
+import com.tinkerpop.blueprints.CloseableIterable;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Element;
 import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.Index;
+import com.tinkerpop.blueprints.IndexableGraph;
+import com.tinkerpop.blueprints.KeyIndexableGraph;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph;
 import com.tinkerpop.blueprints.util.wrappers.batch.BatchGraph;
 
+/**
+ * Create an IndexedGraph object from CyNetwork.
+ * 
+ */
 public class ExportNetworkToDatabaseTask extends AbstractNetworkTask {
 
 	@Tunable(description = "Export to DB:")
@@ -40,6 +52,10 @@ public class ExportNetworkToDatabaseTask extends AbstractNetworkTask {
 		super(appManager.getCurrentNetwork());
 	}
 
+	public ExportNetworkToDatabaseTask(CyNetwork network) {
+		super(network);
+	}
+
 	@Override
 	public void run(TaskMonitor taskMonitor) throws Exception {
 		final Graph graph = new Neo4jGraph(databaseLocation);
@@ -49,48 +65,113 @@ public class ExportNetworkToDatabaseTask extends AbstractNetworkTask {
 		final CyTable nodeTable = network.getDefaultNodeTable();
 		final CyTable edgeTable = network.getDefaultEdgeTable();
 
+		Collection<CyColumn> nodeColumns = createValidColumns(nodeTable);
+		Collection<CyColumn> edgeColumns = createValidColumns(edgeTable);
+
+		// createIdx(nodeColumns, (IndexableGraph) graph, Vertex.class);
+		// createIdx(edgeColumns, (IndexableGraph) graph, Edge.class);
+
 		for (final CyEdge edge : allEdges) {
-			
-			
+
 			final CyNode source = edge.getSource();
 			final CyNode target = edge.getTarget();
 
-			System.out.println("ID = " + edge.getSUID() + ", s = " + source.getSUID() +", t = " + target.getSUID());
-			Vertex sourceV = bgraph.getVertex(source.getSUID());
-			if(sourceV == null)
-				sourceV = bgraph.addVertex(source.getSUID());
-			createProperties(nodeTable, source, sourceV);
-			Vertex targetV = bgraph.getVertex(target.getSUID());
-			if(targetV == null)
-				targetV = bgraph.addVertex(target.getSUID());
-			createProperties(nodeTable, target, targetV);
+			String sourceName = network.getRow(source).get(CyNetwork.NAME,
+					String.class);
+			String targetName = network.getRow(target).get(CyNetwork.NAME,
+					String.class);
+
+			Vertex sourceV = bgraph.getVertex(sourceName);
+			if (sourceV == null)
+				sourceV = bgraph.addVertex(sourceName);
+			createProperties(nodeColumns, source, sourceV);
+
+			Vertex targetV = bgraph.getVertex(targetName);
+			if (targetV == null)
+				targetV = bgraph.addVertex(targetName);
+			createProperties(nodeColumns, target, targetV);
 
 			final String interactionType = network.getRow(edge).get(
 					CyEdge.INTERACTION, String.class);
-			final Edge tEdge = bgraph.addEdge(edge.getSUID(), sourceV, targetV,
+			final Edge tEdge = bgraph.addEdge(null, sourceV, targetV,
 					interactionType);
-			createProperties(edgeTable, edge, tEdge);
+			createProperties(edgeColumns, edge, tEdge);
 		}
+
 		bgraph.shutdown();
+		graph.shutdown();
+
+		KeyIndexableGraph iGraph = new Neo4jGraph(databaseLocation);
+		createIdx(nodeColumns, iGraph, Vertex.class);
+		createIdx(edgeColumns, iGraph, Edge.class);
+		iGraph.shutdown();
+
 	}
 
-	private final void createProperties(final CyTable table,
-			final CyIdentifiable graphObject, final Element element) {
-
-		final CyRow row = network.getRow(graphObject);
-
+	private Collection<CyColumn> createValidColumns(CyTable table) {
 		final Collection<CyColumn> originalColumns = table.getColumns();
 		// Skip List for now... TODO: Implement List to String module
 		final Set<CyColumn> columns = new HashSet<CyColumn>();
 		for (CyColumn col : originalColumns) {
-			if (col.getType() != List.class)
-				columns.add(col);
+			if (col.getType() != List.class) {
+				if (!col.getName().equals(CyNetwork.SUID)
+						&& !col.getName().equals(CyNetwork.SELECTED)) {
+
+					System.out.println("New col = " + col.getName());
+					columns.add(col);
+				}
+			}
 		}
+		return columns;
+	}
+
+	private Map<String, Index<?>> idxMap = new HashMap<String, Index<?>>();
+
+	private void createIdx(Collection<CyColumn> columns,
+			KeyIndexableGraph graph, Class<? extends Element> type) {
+		for (final CyColumn column : columns) {
+			System.out.println("### Making key.. = " + column.getName());
+			graph.createKeyIndex(column.getName(), type);
+
+		}
+		System.out.println("### KEY IDX created = "
+				+ graph.getIndexedKeys(type));
+	}
+
+	private String getIdxName(CyColumn column, Class<? extends Element> type) {
+		return type.getSimpleName() + "." + column.getName();
+	}
+
+	private final void createProperties(Collection<CyColumn> columns,
+			final CyIdentifiable graphObject, final Element element) {
+
+		final CyRow row = network.getRow(graphObject);
 
 		for (final CyColumn column : columns) {
 			final String colName = column.getName();
 			final Object value = row.get(colName, column.getType());
-			element.setProperty(colName, value);
+			if (value != null) {
+				element.setProperty(colName, value);
+
+				// String idxName;
+				// if (element instanceof Vertex)
+				// idxName = getIdxName(column, Vertex.class);
+				// else {
+				// idxName = getIdxName(column, Edge.class);
+				// }
+				//
+				// System.out.println("$$ IDX name = " + idxName);
+				//
+				// final Index idx = idxMap.get(idxName);
+				// System.out.println("$$ GOT IDX = " + idx);
+				// if (idx != null) {
+				// if (element instanceof Vertex)
+				// idx.put(idxName, value, (Vertex)element);
+				// else {
+				// idx.put(idxName, value, (Edge)element);
+				// }
+				// }
+			}
 		}
 	}
 }
--- src/test/java/org/cytoscape/blueprints/ExportGraphTaskTest.java
+++ src/test/java/org/cytoscape/blueprints/ExportGraphTaskTest.java
@@ -1,22 +1,57 @@
 package org.cytoscape.blueprints;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 
+import org.cytoscape.application.CyApplicationManager;
+import org.cytoscape.blueprints.graphdb.internal.ExportNetworkToDatabaseTask;
+import org.cytoscape.ding.NetworkViewTestSupport;
+import org.cytoscape.io.internal.read.sif.SIFNetworkReader;
+import org.cytoscape.io.internal.util.ReadUtils;
+import org.cytoscape.io.internal.util.StreamUtilImpl;
 import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.model.CyNetworkManager;
 import org.cytoscape.model.CyNode;
 import org.cytoscape.model.NetworkTestSupport;
+import org.cytoscape.model.subnetwork.CyRootNetworkManager;
+import org.cytoscape.property.CyProperty;
+import org.cytoscape.property.CyProperty.SavePolicy;
+import org.cytoscape.property.SimpleCyProperty;
+import org.cytoscape.view.layout.CyLayoutAlgorithm;
+import org.cytoscape.view.layout.CyLayoutAlgorithmManager;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.model.CyNetworkViewFactory;
+import org.cytoscape.work.AbstractTask;
+import org.cytoscape.work.TaskIterator;
+import org.cytoscape.work.TaskMonitor;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import scala.xml.NodeSeq;
+
+import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Element;
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Index;
 import com.tinkerpop.blueprints.IndexableGraph;
 import com.tinkerpop.blueprints.KeyIndexableGraph;
+import com.tinkerpop.blueprints.TransactionalGraph;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph;
 import com.tinkerpop.blueprints.impls.neo4j.batch.Neo4jBatchGraph;
@@ -24,62 +59,220 @@ import com.tinkerpop.blueprints.util.wrappers.batch.BatchGraph;
 
 public class ExportGraphTaskTest {
 
+	private static final String DATABASE_LOCATION = "target/dbtest";
+
 	private final NetworkTestSupport testSupport = new NetworkTestSupport();
+	private TaskMonitor taskMonitor;
+	private CyNetworkFactory netFactory;
+	private CyNetworkViewFactory viewFactory;
+	private ReadUtils readUtil;
+	private CyLayoutAlgorithmManager layouts;
+	private CyNetworkManager networkManager;
+	private CyRootNetworkManager rootNetworkManager;
+	private CyApplicationManager cyApplicationManager;
+
+	private Properties properties;
+
+	static class SimpleTask extends AbstractTask {
+		public void run(final TaskMonitor tm) {
+		}
+	}
 
 	@Before
 	public void setUp() throws Exception {
+		taskMonitor = mock(TaskMonitor.class);
+
+		CyLayoutAlgorithm def = mock(CyLayoutAlgorithm.class);
+		Object context = new Object();
+		when(def.createLayoutContext()).thenReturn(context);
+		when(def.getDefaultLayoutContext()).thenReturn(context);
+		when(
+				def.createTaskIterator(Mockito.any(CyNetworkView.class),
+						Mockito.any(Object.class), Mockito.anySet(),
+						Mockito.any(String.class))).thenReturn(
+				new TaskIterator(new SimpleTask()));
+
+		layouts = mock(CyLayoutAlgorithmManager.class);
+		when(layouts.getDefaultLayout()).thenReturn(def);
+
+		NetworkTestSupport nts = new NetworkTestSupport();
+		netFactory = nts.getNetworkFactory();
+
+		networkManager = nts.getNetworkManager();
+		rootNetworkManager = nts.getRootNetworkFactory();
+
+		cyApplicationManager = mock(CyApplicationManager.class);
+
+		properties = new Properties();
+		CyProperty<Properties> cyProperties = new SimpleCyProperty<Properties>(
+				"Test", properties, Properties.class, SavePolicy.DO_NOT_SAVE);
+		NetworkViewTestSupport nvts = new NetworkViewTestSupport();
+
+		viewFactory = nvts.getNetworkViewFactory();
+		readUtil = new ReadUtils(new StreamUtilImpl(cyProperties));
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	private final CyNetwork createGraph(int size) {
-		CyNetwork network = testSupport.getNetwork();
-		for (int i = 0; i < size; i++) {
-			CyNode node1 = network.addNode();
-			CyNode node2 = network.addNode();
-			network.getRow(node1).set(CyNetwork.NAME, "Source " + i);
-			network.getRow(node2).set(CyNetwork.NAME, "Target " + i);
-			CyEdge edge = network.addEdge(node1, node2, true);
-			network.getRow(edge).set(CyNetwork.NAME, "Edge " + i);
-		}
-		return network;
-	}
-
 	@Test
 	public void testExport() throws Exception {
 
-		IndexableGraph bgraph = new Neo4jBatchGraph("target/tmp/neo4jdata");
-		final Index<Vertex> nodeNameIdx = bgraph.createIndex("name", Vertex.class);
-		final Index<Edge> edgeInteractionIdx = bgraph.createIndex("interaction", Edge.class);
-		
-		CyNetwork network = createGraph(1000);
-		assertEquals(1000, network.getEdgeCount());
-		
-		for (CyEdge edge : network.getEdgeList()) {
-			
-			final CyNode source = edge.getSource();
-			final CyNode target = edge.getTarget();
-			
-			Vertex sourceV = bgraph.addVertex(null);
-			Vertex targetV = bgraph.addVertex(null);
+		TransactionalGraph bgraph = new Neo4jGraph(DATABASE_LOCATION);
+		bgraph.shutdown();
 
-			Edge tEdge = bgraph.addEdge(null, sourceV, targetV, "interact_with");	
+		clearDB();
+
+		// Read network from real network file
+		final CyNetwork[] networks = getNetworks("galFiltered.sif");
+		assertNotNull(networks);
+		assertEquals(1, networks.length);
+
+		final CyNetwork network = networks[0];
+		assertNotNull(network);
+
+		final ExportNetworkToDatabaseTask task = new ExportNetworkToDatabaseTask(
+				network);
+		task.databaseLocation = DATABASE_LOCATION;
+		task.run(taskMonitor);
+
+		System.out.println("Insertion finished!");
+
+		testResult();
+
+	}
+
+	private final void testResult() {
+		Graph bgraph = new Neo4jGraph(DATABASE_LOCATION);
+
+		Iterable<Vertex> nodes = bgraph.getVertices();
+		Iterator<Vertex> itr2 = nodes.iterator();
+
+		System.out.println("=== Counting...");
+
+		long nodeCount = countResult(itr2);
+
+		Iterable<Edge> edges = bgraph.getEdges();
+		Iterator<Edge> itr = edges.iterator();
+		long edgeCount = countResult(itr);
+
+		System.out.println("Nodes = " + nodeCount);
+		System.out.println("Edges = " + edgeCount);
+
+		// Number of edges
+		assertEquals(362l, edgeCount);
+
+		// Number of nodes
+		assertEquals(331l, nodeCount);
+
+		testGraphTopology((KeyIndexableGraph) bgraph);
+
+		bgraph.shutdown();
+	}
+
+	private final void testGraphTopology(KeyIndexableGraph graph) {
+
+		Set<String> nodeKeys = graph.getIndexedKeys(Vertex.class);
+		Set<String> edgeKeys = graph.getIndexedKeys(Edge.class);
+		assertEquals(2, nodeKeys.size());
+		assertEquals(4, edgeKeys.size());
+
+		for (String key : nodeKeys) {
+			System.out.println("Node Key = " + key);
+		}
+		for (String key : edgeKeys) {
+			System.out.println("Edge Key = " + key);
 		}
 
+		// Get node by index
+		String hub1 = "YNL216W";
+		Iterable<Vertex> res1 = graph.getVertices(CyNetwork.NAME, hub1);
+		Iterator<Vertex> itr = res1.iterator();
+		assertEquals(1, countResult(itr));
+		Vertex node1 = graph.getVertices(CyNetwork.NAME, hub1).iterator()
+				.next();
+		Iterable<Edge> edges = node1.getEdges(Direction.BOTH);
+		Iterator<Edge> itr2 = edges.iterator();
+		assertEquals(17l, countResult(itr2));
+
+		String[] neighbours = { "YNL216W", "YAL038W", "YBR093C", "YCL030C", "YCR012W",
+				"YDR050C", "YDR171W", "YER074W", "YGR254W", "YHR174W",
+				"YIL069C", "YIL133C", "YLR044C", "YML024W", "YNL301C",
+				"YOL086C", "YOL120C", "YOL127W"};
 		
+		final List<String> nList = Arrays.asList(neighbours);
 		
-		bgraph.shutdown();
+		Iterator<Edge> edges2 = node1.getEdges(Direction.BOTH).iterator();
+		while(edges2.hasNext()) {
+			final Edge edge = edges2.next();
+			final Vertex in = edge.getVertex(Direction.IN);
+			final Vertex out = edge.getVertex(Direction.OUT);
+			assertTrue(nList.contains(in.getProperty(CyNetwork.NAME)) && 
+					nList.contains(out.getProperty(CyNetwork.NAME)) );
 		
-		bgraph = new Neo4jGraph("target/tmp/neo4jdata");
-		Iterable edges = bgraph.getEdges();
-		Iterator itr = edges.iterator();
-		while(itr.hasNext()) {
-			Object e = itr.next();
-			System.out.println("Edge = " + e);
 		}
-		bgraph.shutdown();
+
+	}
+
+	private final long countResult(Iterator<?> itr) {
+		long counter = 0;
+		while (itr.hasNext()) {
+			Object val = itr.next();
+			counter++;
+		}
+		return counter;
+	}
+
+	private final void clearDB() {
+		TransactionalGraph graph = new Neo4jGraph(DATABASE_LOCATION);
+
+		Iterable<Edge> edges = graph.getEdges();
+		Iterator<Edge> itr2 = edges.iterator();
+
+		while (itr2.hasNext()) {
+			Edge edge = itr2.next();
+			graph.removeEdge(edge);
+		}
+
+		Iterable<Vertex> nodes = graph.getVertices();
+		Iterator<Vertex> itr = nodes.iterator();
+
+		while (itr.hasNext()) {
+			Vertex vertex = itr.next();
+			graph.removeVertex(vertex);
+		}
+
+		graph.commit();
+
+		assertEquals(0, countResult(itr));
+		assertEquals(0, countResult(itr2));
+
+		final IndexableGraph iGraph = (IndexableGraph) graph;
+		Iterable<Index<? extends Element>> idxs = iGraph.getIndices();
+		Iterator<Index<? extends Element>> idxItr = idxs.iterator();
+		while (idxItr.hasNext()) {
+			iGraph.dropIndex(idxItr.next().getIndexName());
+		}
+
+		graph.shutdown();
+
+	}
+
+	private SIFNetworkReader readFile(String file) throws Exception {
+		File f = new File("./src/test/resources/" + file);
+		SIFNetworkReader snvp = new SIFNetworkReader(new FileInputStream(f),
+				layouts, viewFactory, netFactory, this.networkManager,
+				this.rootNetworkManager, this.cyApplicationManager);
+		new TaskIterator(snvp);
+		snvp.run(taskMonitor);
+
+		return snvp;
+	}
+
+	private CyNetwork[] getNetworks(String file) throws Exception {
+		final SIFNetworkReader snvp = readFile(file);
+		return snvp.getNetworks();
 	}
 
 }
